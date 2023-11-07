50be127980351c7d5595cbdd9973ba8bba46131d
Elena Karsten <e.karstenzv@gmail.com>
2023-11-07 17:15:08 +0800

    init

---
+++ src/components/withFriendlyDate.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+export default function withFriendlyDate(Component) {
+  class WrappedComponent extends React.Component {
+    static propTypes = {
+      date: PropTypes.string.isRequired,
+    };
+    getFriendlyDateString = date => {
+      if (moment().diff(moment(date), 'day') <= 25) {
+        return moment(date).fromNow();
+      }
+      return `${moment().diff(moment(date), 'day')} дней назад`;
+    }
+
+    render() {
+      const { date } = this.props;
+      const friendlyDate = this.getFriendlyDateString(date);
+      return <Component {...this.props} date={friendlyDate} />;
+    }
+  }
+
+  WrappedComponent.displayName = `withFriendlyDate(${Component.displayName || Component.name})`;
+
+  return WrappedComponent;
+}
